fde19d1eb45c0c1e7859f609ec34e3c417061cb2
Yilmaz SEN <yilmazsen94@gmail.com>
2019-03-30 21:04:33 +0300

    checkpoint2
    Added New Features

--- components/Dashboard/TransactionTable.vue
+++ components/Dashboard/TransactionTable.vue
@@ -1,118 +1,85 @@
 <template>
-<div>
-  <el-table
-   border
-    :data="tableData"
-    style="width: 100%">
+  <div>
+    <el-table border :data="facts" style="width: 100%" v-loading="loading">
+      <el-table-column label="#" type="index"></el-table-column>
 
- <el-table-column
-      label="#"
-        type="index">
-    </el-table-column>
+      <el-table-column label="Staff Name" prop="name" width="100px"></el-table-column>
 
-    <el-table-column
-      label="Staff Name"
-      prop="staffname">
-    </el-table-column>
+      <el-table-column label="LOS" prop="los" width="100px"></el-table-column>
 
- <el-table-column
-      label="LOS"
-      prop="los">
-    </el-table-column>
+      <el-table-column label="Sub LOS" prop="sublos" width="130px"></el-table-column>
 
- <el-table-column
-      label="Sub LOS"
-      prop="sublos">
-    </el-table-column>
- 
- <el-table-column
-      label="Bottom LOS"
-      prop="ssublos">
-    </el-table-column>
- <el-table-column
-      label="Title"
-      prop="title">
-    </el-table-column>
+      <el-table-column label="Bottom LOS" prop="bottomlos"></el-table-column>
+      <el-table-column label="Title" prop="titles"></el-table-column>
 
-  <el-table-column
-      label="Location"
-      prop="location">
-    </el-table-column>
-        <el-table-column
-      label="Date"
-      prop="date">
-    </el-table-column>
+      <el-table-column label="Location" prop="location" width="75px"></el-table-column>
+      <el-table-column label="Date" prop="date1" width="99px"></el-table-column>
 
-    <el-table-column
-      align="right">
-      <template slot="header">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="Type to search"/>
-      </template>
+      <el-table-column align="right">
+        <template slot="header">
+          <el-input size="mini" placeholder="Type to search"/>
+        </template>
 
-      <template >
-        <el-button
-          size="mini"
-            type="primary"
-           icon="el-icon-edit" >Edit</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-         @click="dialogVisible = true" icon="el-icon-delete">Delete</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+        <template slot-scope="scope">
+          <el-button size="mini" type="primary" icon="el-icon-edit">Edit</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click.native.prevent="deleteRow(scope.$index,facts)"
+            icon="el-icon-delete"
+          >Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-
-  <el-dialog
-  title="Delete Operation"
-  :visible.sync="dialogVisible"
-  width="30%"
-  >
-  <span><b>Are you sure that you want to delete this record?</b></span>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">Cancel</el-button>
-    <el-button type="success" @click="dialogVisible = false">Confirm</el-button>
-  </span>
-</el-dialog>
-
-</div>
-
+    <el-dialog title="Delete Operation" :visible.sync="dialogVisible" width="30%">
+      <span>
+        <b>Are you sure that you want to delete this record?</b>
+      </span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="success" @click="dialogVisible = false">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-       
+import axios from "axios";
+export default {
+  data() {
+    return {
+      loading: true,
+      items: [],
+      facts: [],
+      search: "",
+      dialogVisible: false
+      //   tableData: Array().fill(facts),
+      //     search: '',
+      //     dialogVisible: false
+    };
+  },
 
-      const item = {
-       
-        los: 'IFS',
-        sublos: 'IT',
-        ssublos: 'Support',
-        staffname: 'Yilmaz SEN',
-        title: 'IT Specialist',
-        location:'Beşiktaş',
-         date: '2019-02-16'
-      };
-      return {
-        tableData: Array(20).fill(item),
-          search: '',
-          dialogVisible: false
-      }
- 
+  async created() {
+    await axios
+      .get("https://pwcdemo-1c4d3.firebaseio.com/users.json")
+      .then(response => {
+        (this.loading = false), (this.facts = Object.values(response.data));
+      });
+    console.log(Object.values(this.facts));
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("Are you sure to close this dialog?")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
     },
-    methods: {
-      handleClose(done) {
-        this.$confirm('Are you sure to close this dialog?')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
-      }
-    },
+    deleteRow(index, facts) {
+      facts.splice(index, 1);
+    }
   }
+};
 </script>
 
